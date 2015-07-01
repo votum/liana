0f3d7b13cc55b1c35bee7e2d0a1763585447f0c7
maersu <me@maersu.ch>
2015-07-01 17:45:13 +0200

    reduce max

--- server/api/pagination.py
+++ server/api/pagination.py
@@ -8,4 +8,4 @@ from rest_framework.pagination import PageNumberPagination
 class LargeResultsSetPagination(PageNumberPagination):
     page_size = 2000
     page_size_query_param = 'page_size'
-    max_page_size = 50000
+    max_page_size = 10000
